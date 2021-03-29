5e95322f640f3b7b647283a2ab6d81c07a1fa94c
Hagen Wierstorf <hwierstorf@audeering.com>
2021-03-29 08:49:20 +0200

    TST: speed up cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,24 +10,23 @@ import pytest
             '1.0.1',
             'emotion',
         ),
-        (
-            'emodb',
-            '1.0.1',
-            'files',
-        ),
     ],
 )
 def test_audb2(script_runner, name, table, version):
 
     ret = script_runner.run(
-        'audb2', 'load', name, '--version', version,
+        'audb2', 'load', name,
+        '--version', version,
+        '--only_metadata', 'True',
         '--verbose', 'False',
     )
     assert ret.success
     assert ret.stderr == ''
 
     ret = script_runner.run(
-        'audb2get', name, table, '--version', version,
+        'audb2get', name, table,
+        '--version', version,
+        '--only_metadata', 'True',
         '--verbose', 'False',
     )
     assert ret.success
