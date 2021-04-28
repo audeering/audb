c7adf4b35f8f7e3884e0c707ffeb305ac6ff2c66
Hagen Wierstorf <hwierstorf@audeering.com>
2021-04-28 09:58:21 +0200

    Add docstring to new conftest

--- audb/core/conftest.py
+++ audb/core/conftest.py
@@ -5,6 +5,15 @@ import audb
 
 @pytest.fixture(autouse=True)
 def add_audb_with_public_data(doctest_namespace):
+    r"""Provide access to the public Artifactory repository.
+
+    Some tests in the docstrings need access to the emodb database.
+    As all the unit tests defined under ``tests/*``
+    should not be able to see the public repository
+    as the number of available databases would then not be deterministic,
+    we have to add the extra ``conftest.py`` file here instead.
+
+    """
     audb.config.REPOSITORIES = [
         audb.Repository(
             name='data-public',
