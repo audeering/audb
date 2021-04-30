5511bc99dae1cafb361eec230e7420c62dd291de
Hagen Wierstorf <hwierstorf@audeering.com>
2021-04-30 08:40:18 +0200

    Mark deprecated removal for version 1.2.0 (#66)

--- audb/core/backward.py
+++ audb/core/backward.py
@@ -6,7 +6,7 @@ from audb.core.api import default_cache_root
 
 
 @audeer.deprecated(
-    removal_version='1.1.0',
+    removal_version='1.2.0',
     alternative='default_cache_root',
 )
 def get_default_cache_root() -> str:
