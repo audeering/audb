23dae865371914c0f591850030f4f557b81b8ad6
Hagen Wierstorf <hwierstorf@audeering.com>
2021-07-23 16:01:59 +0200

    Fix future warnings of pandas

--- audb/core/load.py
+++ audb/core/load.py
@@ -177,12 +177,19 @@ def _fix_media_ext(
         cur_ext = r'\.[a-zA-Z0-9]+$'  # match file extension
         new_ext = f'.{format}'
         if table.is_filewise:
-            table.df.index = table.df.index.str.replace(cur_ext, new_ext)
+            table.df.index = table.df.index.str.replace(
+                cur_ext,
+                new_ext,
+                regex=True,
+            )
         else:
-            table.df.index.set_levels(
-                table.df.index.levels[0].str.replace(cur_ext, new_ext),
-                'file',
-                inplace=True,
+            table.df.index = table.df.index.set_levels(
+                table.df.index.levels[0].str.replace(
+                    cur_ext,
+                    new_ext,
+                    regex=True,
+                ),
+                level='file',
             )
 
     audeer.run_tasks(
