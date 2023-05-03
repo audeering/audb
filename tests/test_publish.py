c21d0404d131b3ae637c270a57142ae81c89fda8
beddine <beddine@audeering.com>
2023-05-03 16:06:39 +0200

    Change tests order

--- tests/test_publish.py
+++ tests/test_publish.py
@@ -360,53 +360,6 @@ def test_invalid_archives(dbs, persistent_repository, name):
         )
 
 
-def test_media_files_order_stability_during_publication():
-    media_files = {}
-    n_iterations = 10
-    for i in range(n_iterations):
-        with tempfile.TemporaryDirectory() as tmp:
-            name = 'db'
-            version = '1.0.0'
-
-            host = audeer.mkdir(audeer.path(tmp, 'host'))
-            db_root = audeer.mkdir(audeer.path(tmp, name))
-            cache_root = audeer.mkdir(audeer.path(tmp, 'cache'))
-
-            sampling_rate = 8000
-            duration = 1
-            signal = np.zeros((1, duration * sampling_rate))
-            audiofile.write(
-                audeer.path(db_root, 'f0.wav'), signal, sampling_rate
-            )
-            audiofile.write(
-                audeer.path(db_root, 'f1.wav'), signal, sampling_rate
-            )
-
-            db = audformat.Database(name)
-            index = audformat.filewise_index(['f0.wav', 'f1.wav'])
-            db['table'] = audformat.Table(index)
-            db['table']['column'] = audformat.Column()
-            db['table']['column'].set(['label0', 'label1'])
-            db.save(db_root)
-
-            repository = audb.Repository(
-                name='repo',
-                host=host,
-                backend='file-system',
-            )
-            audb.config.REPOSITORIES = [repository]
-            audb.config.CACHE_ROOT = cache_root
-
-            audb.publish(db_root, version, repository)
-
-            deps = audb.dependencies(name, version=version)
-
-            media_files[i] = deps()
-
-    for i in range(n_iterations - 1):
-        pd.testing.assert_frame_equal(media_files[i], media_files[i + 1])
-
-
 @pytest.mark.parametrize(
     'version',
     [
@@ -1337,3 +1290,50 @@ def test_update_database_without_media(tmpdir, persistent_repository):
             audeer.path(build_root, attachment_file),
             audeer.path(db_load.root, attachment_file),
         )
+
+
+def test_publish_media_files_order_stability():
+    media_files = {}
+    n_iterations = 10
+    for i in range(n_iterations):
+        with tempfile.TemporaryDirectory() as tmp:
+            name = 'db'
+            version = '1.0.0'
+
+            host = audeer.mkdir(audeer.path(tmp, 'host'))
+            db_root = audeer.mkdir(audeer.path(tmp, name))
+            cache_root = audeer.mkdir(audeer.path(tmp, 'cache'))
+
+            sampling_rate = 8000
+            duration = 1
+            signal = np.zeros((1, duration * sampling_rate))
+            audiofile.write(
+                audeer.path(db_root, 'f0.wav'), signal, sampling_rate
+            )
+            audiofile.write(
+                audeer.path(db_root, 'f1.wav'), signal, sampling_rate
+            )
+
+            db = audformat.Database(name)
+            index = audformat.filewise_index(['f0.wav', 'f1.wav'])
+            db['table'] = audformat.Table(index)
+            db['table']['column'] = audformat.Column()
+            db['table']['column'].set(['label0', 'label1'])
+            db.save(db_root)
+
+            repository = audb.Repository(
+                name='repo',
+                host=host,
+                backend='file-system',
+            )
+            audb.config.REPOSITORIES = [repository]
+            audb.config.CACHE_ROOT = cache_root
+
+            audb.publish(db_root, version, repository)
+
+            deps = audb.dependencies(name, version=version)
+
+            media_files[i] = deps()
+
+    for i in range(n_iterations - 1):
+        pd.testing.assert_frame_equal(media_files[i], media_files[i + 1])
