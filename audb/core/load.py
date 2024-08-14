2b9953db426f87939ee7c3f0e144feaacffd0982
Hagen Wierstorf <hwierstorf@audeering.com>
2024-08-14 12:31:15 +0200

    Add audb.stream()

--- audb/core/load.py
+++ audb/core/load.py
@@ -1776,3 +1776,169 @@ def load_table(
         df = db[table].get(map=map)
 
     return df
+
+
+def stream(
+    name: str,
+    table: str,
+    *,
+    version: str = None,
+    batch_size: int = 16,
+    shuffle: bool = False,
+    buffer_size: int = 10_000,
+    only_metadata: bool = False,
+    bit_depth: int = None,
+    channels: typing.Union[int, typing.Sequence[int]] = None,
+    format: str = None,
+    mixdown: bool = False,
+    sampling_rate: int = None,
+    attachments: typing.Union[str, typing.Sequence[str]] = None,
+    full_path: bool = True,
+    cache_root: str = None,
+    num_workers: typing.Optional[int] = 1,
+    timeout: float = -1,
+    verbose: bool = True,
+) -> typing.Optional[audformat.Database]:
+    r"""Stream table and media files of a database.
+
+    Loads only the first ``batch_size`` rows
+    of a table into memory,
+    and downloads only the related media files,
+    if any.
+
+    By setting
+    ``bit_depth``,
+    ``channels``,
+    ``format``,
+    ``mixdown``,
+    and ``sampling_rate``
+    we can request a specific flavor of the database.
+    In that case media files are automatically converted to the desired
+    properties (see also :class:`audb.Flavor`).
+
+    Args:
+        name: name of database
+        table: name of table
+        version: version string, latest if ``None``
+        batch_size: number of table rows
+            to return in one iteration
+        shuffle: if ``True``,
+            it first reads ``buffer_size`` rows from the table
+            and selects ``batch_size`` randomly from them
+        buffer_size: number of table rows
+            to be loaded
+            when ``shuffle`` is ``True``
+        only_metadata: load only header and tables of database
+        bit_depth: bit depth, one of ``16``, ``24``, ``32``
+        channels: channel selection, see :func:`audresample.remix`.
+            Note that media files with too few channels
+            will be first upsampled by repeating the existing channels.
+            E.g. ``channels=[0, 1]`` upsamples all mono files to stereo,
+            and ``channels=[1]`` returns the second channel
+            of all multi-channel files
+            and all mono files
+        format: file format, one of ``'flac'``, ``'wav'``
+        mixdown: apply mono mix-down
+        sampling_rate: sampling rate in Hz, one of
+            ``8000``, ``16000``, ``22500``, ``44100``, ``48000``
+        attachments: load only attachment files
+            for the attachments
+            matching the regular expression
+            or provided in the list.
+            If set to ``[]`` no attachments are loaded
+        full_path: replace relative with absolute file paths
+        cache_root: cache folder where databases are stored.
+            If not set :meth:`audb.default_cache_root` is used
+        num_workers: number of parallel jobs or 1 for sequential
+            processing. If ``None`` will be set to the number of
+            processors on the machine multiplied by 5
+        timeout: maximum wait time if another thread or process is already
+            accessing the database. If timeout is reached, ``None`` is
+            returned. If timeout < 0 the method will block until the
+            database can be accessed
+        verbose: show debug messages
+
+    Returns:
+        database object
+
+    Raises:
+        ValueError: if attachment, table or media is requested
+            that is not part of the database
+        ValueError: if a non-supported ``bit_depth``,
+            ``format``,
+            or ``sampling_rate``
+            is requested
+        RuntimeError: if a flavor is requested,
+            but the database contains media files,
+            that don't contain audio,
+            e.g. text files
+
+    Examples:
+        >>> db = audb.load(
+        ...     "emodb",
+        ...     version="1.4.1",
+        ...     tables=["emotion", "files"],
+        ...     only_metadata=True,
+        ...     full_path=False,
+        ...     verbose=False,
+        ... )
+        >>> list(db.tables)
+        ['emotion', 'files']
+
+    """
+    if version is None:
+        version = latest_version(name)
+
+    db_root = database_cache_root(name, version, cache_root)
+
+    deps = dependencies(
+        name,
+        version=version,
+        cache_root=cache_root,
+        verbose=verbose,
+    )
+
+    if table not in deps.table_ids:
+        msg = error_message_missing_object("table", [table], name, version)
+        raise ValueError(msg)
+
+    with FolderLock(db_root):
+        # Start with database header without tables
+        db, backend_interface = load_header_to(db_root, name, version)
+
+        # Load misc tables completely
+        for _table in _misc_tables_used_in_scheme(db):
+            table_file = os.path.join(db_root, f"db.{_table}")
+            if not (
+                os.path.exists(f"{table_file}.csv")
+                or os.path.exists(f"{table_file}.pkl")
+            ):
+                pass
+
+        # Load table
+        tables = _misc_tables_used_in_scheme(db) + [table]
+        for _table in tables:
+            table_file = os.path.join(db_root, f"db.{_table}")
+            if not (
+                os.path.exists(f"{table_file}.csv")
+                or os.path.exists(f"{table_file}.pkl")
+            ):
+                _load_files(
+                    [_table],
+                    "table",
+                    backend_interface,
+                    db_root,
+                    db,
+                    version,
+                    None,
+                    deps,
+                    Flavor(),
+                    cache_root,
+                    pickle_tables,
+                    num_workers,
+                    verbose,
+                )
+            db[_table].load(table_file)
+
+    return db[table]._df
+>>>>>>> a9d7784 (Add audb.stream())
