681e4825f0ca19dcadc890e8623906d7d863e41c
Hagen Wierstorf <hwierstorf@audeering.com>
2025-05-28 14:37:35 +0200

    TST: ensure default config values for cache

--- audb/core/conftest.py
+++ audb/core/conftest.py
@@ -22,28 +22,35 @@ def cache(tmpdir_factory):
     it makes sense to cache it
     across all docstring tests.
 
+    We use a environment variable,
+    and set ``audb.config.CACHE_ROOT`` to its default value.
+
     """
     cache = tmpdir_factory.mktemp("cache")
-    # We use the environment variable here
-    # to ensure audb.config.CACHE_ROOT
-    # does still return the default config value
-    # in the doctest
+    # Store original values
     env_cache = os.environ.get("AUDB_CACHE_ROOT", None)
     env_shared_cache = os.environ.get("AUDB_SHARED_CACHE_ROOT", None)
+    config_cache = audb.config.CACHE_ROOT
+    config_shared_cache = audb.config.SHARED_CACHE_ROOT
+    # Assign values for test
     os.environ["AUDB_CACHE_ROOT"] = str(cache)
     os.environ["AUDB_SHARED_CACHE_ROOT"] = str(cache)
+    audb.config.CACHE_ROOT = "~/audb"
+    audb.config.SHARED_CACHE_ROOT = "/data/audb"
 
     yield
 
+    # Reassign original values
     if env_cache is None:
         del os.environ["AUDB_CACHE_ROOT"]
     else:  # pragma: nocover
         os.environ["AUDB_CACHE_ROOT"] = env_cache
-
     if env_shared_cache is None:
         del os.environ["AUDB_SHARED_CACHE_ROOT"]
     else:  # pragma: nocover
         os.environ["AUDB_SHARED_CACHE_ROOT"] = env_shared_cache
+    audb.config.CACHE_ROOT = config_cache
+    audb.config.SHARED_CACHE_ROOT = config_shared_cache
 
 
 @pytest.fixture(autouse=True)
