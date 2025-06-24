fb00bbe072284ffe7fa95f5b83de3022af4c1bb5
Hagen Wierstorf <hwierstorf@audeering.com>
2025-06-24 11:50:45 +0200

    TST: remove test for harmless deps load errors (#507)
    * TST: new failing deps pickle test for pandas 2.3
    
    * Improve comment
    
    * DEBUG
    
    * Remove print statement
    
    * Revert pandas dependency change
    
    * Remove test completely
    
    * Remove corresponding assets files
    
    * Improve comment

--- audb/core/api.py
+++ audb/core/api.py
@@ -301,6 +301,16 @@ def dependencies(
             deps.load(cached_deps_file)
         except Exception:  # does not catch KeyboardInterupt
             # If loading cached file fails, load again from backend
+            #
+            # Loading a cache file can fail
+            # as we use PyArrow data types,
+            # which when loading from pickle
+            # are not compatible between all pandas versions.
+            # We had originally some tests for it,
+            # but as the actual failure is not that important,
+            # we removed them in
+            # See https://github.com/audeering/audb/pull/507
+            #
             backend_interface = utils.lookup_backend(name, version)
             deps = download_dependencies(backend_interface, name, version, verbose)
             # Store as pickle in cache
