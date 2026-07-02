2f0e6d5808335fa6dac608cb6f8ff3072ca6ba65
Hagen Wierstorf <hwierstorf@audeering.com>
2026-07-02 13:33:53 +0200

    Remove deprecated flavor sampling rate of 22500 (#579)
    * Remove deprecated flavor sampling rate of 22500
    
    * Fix typo in error message

--- audb/core/flavor.py
+++ audb/core/flavor.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from collections.abc import Sequence
 import os
 import shutil
-import warnings
 
 import numpy as np
 
@@ -80,16 +79,9 @@ class Flavor(audobject.Object):
 
         if sampling_rate is not None:
             sampling_rate = int(sampling_rate)
-            if sampling_rate == 22500:
-                message = (
-                    "A sampling rate of 22500 Hz is deprecated "
-                    "and will be removed with version 1.13.0 of audb. "
-                    "Use 22050 Hz instead."
-                )
-                warnings.warn(message, category=UserWarning, stacklevel=2)
-            elif sampling_rate not in define.SAMPLING_RATES:
+            if sampling_rate not in define.SAMPLING_RATES:
                 raise ValueError(
-                    f"Sampling_rate has to be one of "
+                    f"sampling_rate has to be one of "
                     f"{define.SAMPLING_RATES}, not {sampling_rate}."
                 )
 
